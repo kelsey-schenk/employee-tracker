e4ec3c7fdb8c14d7e761856fcc7464b3afa41f19
kelsey-schenk <kelsey.schenk@att.net>
2022-01-14 17:42:39 -0500

    fixed switch statement

--- db/index.js
+++ db/index.js
@@ -9,6 +9,11 @@ class Database {
             "SELECT employee.id, employee.first_name,employee.last_name, role.title, department.name as department,role.salary, CONCAT(manager.first_name, '', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN deparment on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         )
     }
+    createEmployee(employee){
+        return this.connection.promise.query(
+            "INSERT INTO employee SET ?", employee
+        )
+    }
 }
 
-module.exports = 
+module.exports = new Database(connection);
--- index.js
+++ index.js
@@ -9,11 +9,16 @@ const promptQuestions = () => {
             name: 'options',
             message: 'What would you like to do? (Use arrow keys)',
             choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
-            validate: optionsInput => {
-                if ('View All Employees') {
-                    return findAllEmployees();
-                }
-            }
         }
     ])
+    .then ((optionsInput) => {
+        switch (optionsInput) {
+        case 'View All Employees':
+            return findAllEmployees();
+        case 'Add Employee':
+            return
+        case 'Update Employee Role':
+            return
+        }
+    })
 }
